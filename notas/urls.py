7fc717df2858960bd7aa47d60dfeb3b9a1a837c2
ADRIANO <adrianooalmeidaa@outlook.com>
2026-02-21 16:10:47 -0300

    Atualizando os arquivos

--- notas/urls.py
+++ notas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from notas.views import index
+from notas.views import index, dashboard
+
 
 urlpatterns = [
-  path('', index,name='index'),
+    path('', index, name='index'),          # Página inicial
+    path('dashboard/', dashboard, name='dash'),
 ]
--- notas/views.py
+++ notas/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-
-
+def dashboard(request):
+    return render(request, 'dashboard.html')
